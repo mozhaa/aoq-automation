c5295b2c6784bf161ea5a14c2a9526bee440793e
mozhaa <mozhay2005@gmail.com>
2024-10-22 21:33:39 +0300

    db

--- db/start.sql
+++ db/start.sql
@@ -4,52 +4,56 @@ CREATE TABLE IF NOT EXISTS Anime(
     title_en TEXT,
     title_ro TEXT,
     title_ru TEXT,
-    mal_url TEXT,
-    shiki_url TEXT,
-    mal_poster_url TEXT,
-    shiki_poster_url TEXT,
-    shiki_poster_thumbnail_url TEXT,
     aired_start TIMESTAMP,
     aired_end TIMESTAMP,
+    mal_url TEXT,
+    shiki_url TEXT,
+    shiki_poster_url TEXT,
+    shiki_poster_thumbnail_url TEXT,
     shiki_rating REAL,
     shiki_rating_count INTEGER,
-    shiki_plan_to_watch INTEGER DEFAULT 0,
-    shiki_completed INTEGER DEFAULT 0,
-    shiki_watching INTEGER DEFAULT 0,
-    shiki_dropped INTEGER DEFAULT 0,
-    shiki_on_hold INTEGER DEFAULT 0,
+    shiki_plan_to_watch INTEGER,
+    shiki_completed INTEGER,
+    shiki_watching INTEGER,
+    shiki_dropped INTEGER,
+    shiki_on_hold INTEGER,
     shiki_favorites INTEGER,
     shiki_comments INTEGER,
+    mal_poster_url TEXT,
     mal_rating REAL,
     mal_favorites INTEGER,
     mal_popularity INTEGER,
     mal_ranked INTEGER,
-    mal_plan_to_watch INTEGER DEFAULT 0,
-    mal_completed INTEGER DEFAULT 0,
-    mal_watching INTEGER DEFAULT 0,
-    mal_dropped INTEGER DEFAULT 0,
-    mal_on_hold INTEGER DEFAULT 0
+    mal_plan_to_watch INTEGER,
+    mal_completed INTEGER,
+    mal_watching INTEGER,
+    mal_dropped INTEGER,
+    mal_on_hold INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS QItem(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     anime_id INTEGER REFERENCES Anime(id),
-    type INTEGER, -- op, ed
-    num INTEGER
+    item_type INTEGER, -- 0 = op, 1 = ed
+    num INTEGER,
+    song_name TEXT,
+    song_artist TEXT,
+    episodes TEXT,
+    state INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS QItemSource(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     qitem_id INTEGER REFERENCES QItem(id),
     priority INTEGER,
-    type INTEGER, -- youtube, torrent, file
+    source_type INTEGER, -- 0 = youtube, 1 = torrent, 2 = file
     path TEXT
 );
 
 CREATE TABLE IF NOT EXISTS QitemSourceTiming(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     qitemsource_id INTEGER REFERENCES QItemSource(id),
-    type INTEGER, -- manual, auto
+    author TEXT, -- manual, auto
     guess_time REAL,
     reveal_time REAL
 );
@@ -57,6 +61,6 @@ CREATE TABLE IF NOT EXISTS QitemSourceTiming(
 CREATE TABLE IF NOT EXISTS QItemDifficulty(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     qitem_id INTEGER REFERENCES QItem(id),
-    author INTEGER, -- auto, id***, ...
+    author TEXT,
     value INTEGER -- very easy, easy, medium, hard, very hard
 );
